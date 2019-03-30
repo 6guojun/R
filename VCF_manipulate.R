b9623dcf1410e83a3189bc5094d35b1ad44b9e52
Guojun Liu <gjliu0325@gmail.com>
2019-03-30 14:54:26 +0500

    this is my first commit

---
+++ VCF_manipulate.R
@@ -0,0 +1,8 @@
+library(vcfR)
+vcf_file=""
+setwd("/Users/liu/02doing")
+vcf_field='SHCHVD26042018_filtered.vcf.recode.vcf'
+vcf <- read.vcfR(vcf_field, verbose = FALSE)
+vcf
+colnames(vcf@fix)
+vcf
